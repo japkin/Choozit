61ffd563c454799b6e1c021de336742220564ea8
japkin <japkin@earthlink.net>
2015-03-04 16:11:34 -0500

    Changing Section names

--- app/src/main/java/com/greatmindsdev/android/choozit/MainActivity.java
+++ app/src/main/java/com/greatmindsdev/android/choozit/MainActivity.java
@@ -1,22 +1,20 @@
 package com.greatmindsdev.android.choozit;
 
-import java.util.Locale;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import java.util.Locale;
 
 
 
@@ -153,7 +151,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
                 case 2:
                     return getString(R.string.title_section3).toUpperCase(l);
                 case 3:
-                    return getString(R.string.title_section3).toUpperCase(l);
+                    return getString(R.string.title_section4).toUpperCase(l);
             }
             return null;
         }
